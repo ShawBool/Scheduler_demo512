9b05b55b1589a38ca9568f2a900b337b8c856370
xinagbuer <1813654811@qq.com>
2026-03-25 13:42:22 +0800

    444444444大刀阔斧的重构

--- src/scheduler/__init__.py
+++ src/scheduler/__init__.py
@@ -7,22 +7,6 @@ from .pipeline import run_pipeline
 from .planner import plan_baseline
 from .replanner import evaluate_replan_trigger
 
-try:
-    from .simulation import generate_task_pool
-except ImportError as exc:
-    module_missing = isinstance(exc, ModuleNotFoundError) and exc.name in {
-        "scheduler.simulation",
-        "simulation",
-    }
-    symbol_missing = (
-        "cannot import name 'generate_task_pool'" in str(exc)
-        and "scheduler.simulation" in str(exc)
-    )
-    if module_missing or symbol_missing:
-        generate_task_pool = None
-    else:
-        raise
-
 __all__ = [
     "Task",
     "ResourceSnapshot",
